037b086c500c86e96f0dce07063d7a21f0f47079
nathan boyette <nathan@natakmesh.com>
2025-04-15 19:52:41 -0400

    fix for systemd stuff, tested good on 2 nodes

--- reticulum_mesh/start_reticulum_stack.sh
+++ reticulum_mesh/start_reticulum_stack.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/natak/reticulum_mesh/tak_transmission/reticulum_module/new_implementation
+export PYTHONPATH=$PYTHONPATH:/home/natak/.local/lib/python3.11/site-packages
 exec python3 -u test_setup.py
