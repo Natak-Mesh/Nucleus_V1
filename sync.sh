a50a9bf6245c04ef0bc51271016e259df78863f9
Nathan Boyette <nathan@natakmesh.com>
2025-06-19 14:28:30 -0400

    create sync script

---
+++ sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copy mesh directory and all contents to git repo
+cp -r ~/mesh ~/git/home/natak/
+
+# Copy mesh_monitor directory and all contents to git repo
+cp -r ~/mesh_monitor ~/git/home/natak/
