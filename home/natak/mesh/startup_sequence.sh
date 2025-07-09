6e9a66f54db0b81208d53754fcbdb8cd2c7d897d
Nathan Boyette <74009174+Nate-Boyette@users.noreply.github.com>
2025-07-09 17:28:06 -0400

    Update startup_sequence.sh

--- home/natak/mesh/startup_sequence.sh
+++ home/natak/mesh/startup_sequence.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
 # Mesh Startup Sequence Script
-# Runs atak_module, macsec.sh, and batmesh.sh in proper order
-
 # Enable error reporting
 set -e
 
 echo "Starting mesh startup sequence..."
 
-# Start atak_module in background
-#echo "Starting ATAK module..."
-#cd /home/natak/meshtastic && python3 atak_module.py &
-#ATAK_PID=$!
-
-# Give atak_module time to initialize
-sleep 2
-
-# Run macsec setup (with sudo)
-#echo "Running MACSec setup..."
-#sudo /home/natak/mesh/macsec.sh
-
+#wpa_supplicant runs in batmesh.sh
 #wpa_supplicant -B -i wlan1 -c /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
 
 # Brief pause between network setup steps
