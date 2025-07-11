eaee2aa636f5f6fcc5f14e03bd92f4081bda5f1d
Nathan Boyette <74009174+Nate-Boyette@users.noreply.github.com>
2025-07-11 19:25:17 -0400

    Update batmesh.sh

--- home/natak/mesh/batmesh.sh
+++ home/natak/mesh/batmesh.sh
@@ -3,11 +3,11 @@
 # Add debug output
 #set -x
 
+echo "batmesh: Starting setup $(date)" >> /var/log/batmesh.log
+
 # Clean up any existing batman interface
 sudo ip link del bat0 2>/dev/null || true
 
-# Enable IP forwarding for routing between interfaces
-sudo sysctl -w net.ipv4.ip_forward=1
 
 # Set interfaces to not be managed by NetworkManager
 nmcli device set eth0 managed no
@@ -17,20 +17,29 @@ nmcli device set br0 managed no
 #sudo ip link set dev wlan1 mtu 1532
 
 #wpa_supplicant sets up mesh and disables HWMP routing
+echo "batmesh: Starting wpa_supplicant" >> /var/log/batmesh.log
 sudo wpa_supplicant -B -i wlan1 -c /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
+echo "batmesh: wpa_supplicant started" >> /var/log/batmesh.log
 
 sleep 5
 #iw wlan1 mesh_param mesh_auto_open_plinks 1
 
 
 # Load batman-adv and set routing algorithm
+echo "batmesh: Loading batman-adv" >> /var/log/batmesh.log
 sudo modprobe batman-adv
+echo "batmesh: Setting ra to BATMAN_V" >> /var/log/batmesh.log
 sudo batctl ra BATMAN_V
+echo "batmesh: ra result: $(sudo batctl ra)" >> /var/log/batmesh.log
+echo "batmesh: Creating bat0 interface" >> /var/log/batmesh.log
 sudo ip link add bat0 type batadv
+echo "batmesh: Adding wlan1 to bat0" >> /var/log/batmesh.log
 sudo ip link set dev wlan1 master bat0
+echo "batmesh: Bringing up interfaces" >> /var/log/batmesh.log
 sudo ip link set dev bat0 up
 sudo ip link set dev br0 up
 sudo ip link set dev bat0 master br0
+echo "batmesh: Interfaces configured" >> /var/log/batmesh.log
 
 # Configure batman-adv
 nmcli device set bat0 managed no
@@ -49,4 +58,6 @@ batctl nc 1
 batctl dat 1
 
 # Restart networking
+echo "batmesh: Restarting systemd-networkd" >> /var/log/batmesh.log
 systemctl restart systemd-networkd
+echo "batmesh: Setup completed $(date)" >> /var/log/batmesh.log
