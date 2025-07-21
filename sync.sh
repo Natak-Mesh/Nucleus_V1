87d2128bc8e75f6de5413267d965fe918387ad73
Nathan Boyette <74009174+Nate-Boyette@users.noreply.github.com>
2025-07-21 11:07:10 -0400

    Delete sync.sh

--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copy mesh directory and all contents to git repo
-cp -r ~/mesh ~/git/home/natak/
-
-# Copy mesh_monitor directory and all contents to git repo
-cp -r ~/mesh_monitor ~/git/home/natak/
